652fe74deaef34f554e2cdba249ed44bf742ad70
Richie Jr <60266955+RichieMontalvo@users.noreply.github.com>
2020-12-02 21:48:39 -0500

    adding necessary files

--- App.js
+++ App.js
@@ -1,21 +1,130 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import { Alert, StyleSheet, View } from 'react-native';
+import { Button } from 'react-native-elements';
+
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import ItemList from './components/ItemList';
+import ItemDetails from './components/ItemDetails';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+
+  const [cameraPermission, askForCameraPermission] = Permissions.usePermissions(Permissions.CAMERA, {ask: true});
+  const [cameraRollPermission, askForCameraRollPermission] = Permissions.usePermissions(Permissions.CAMERA_ROLL, {ask: true});
+
+  const [items, setItems] = useState([]);
+
+  useEffect ( () => {
+    if (items.length != 0)
+    storeData(items);
+  });
+
+  const storeData = async (items) => {
+    try {
+      const json = JSON.stringify(items);
+      await AsyncStorage.setItem('@items', json);
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+
+  const pickPhoto = async () => {
+    if (cameraRollPermission.status == 'granted') {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
+        base64: true
+      });
+      if (result.base64) {
+        const item = {
+          image: result.base64,
+        }
+        setItems([...items, item]);
+      }
+    }
+    else {
+      askForCameraRollPermission();
+    }
+  }
+
+  const takePhoto = async () => {
+    if (cameraPermission.status == 'granted') {
+      let result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
+        base64: true,
+      });
+
+      if (result.uri) {
+        if (cameraRollPermission.status == 'granted') {
+          MediaLibrary.saveToLibraryAsync(result.uri);
+          const item = {
+            image: result.base64,
+          }
+          setItems([...items, item]);
+        }
+      }
+
+    }
+    else {
+      askForCameraPermission();
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Item List" 
+          component={ItemList} 
+          options={{
+            headerRight: () => (
+              <View style={styles.buttons}>
+                <Button 
+                  title="Camera"
+                  onPress={takePhoto}
+                />
+
+                <Button 
+                  title="Library"
+                  onPress={pickPhoto}
+                  buttonStyle={styles.libraryButton}
+                />
+              </View>
+            )
+          }}
+        />
+
+        <Stack.Screen name="Item Details" component={ItemDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
+  buttons: {
+    marginRight: 5,
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
-});
+  libraryButton: {
+    marginLeft: 5,
+  }
+})
---
+++ components/ItemList.js
@@ -0,0 +1,95 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, TouchableOpacity, Image } from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const ItemList = ({ route, navigation }) => {
+
+  const [items, setItems] = useState([]);
+
+  useEffect( () => {
+    //console.log(items);
+    getData();
+  })
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('@items');
+      if (jsonValue != null) {
+        setItems(JSON.parse(jsonValue))
+      }
+    }
+    catch(e) {
+      console.log(e);
+    }
+  }
+
+  const goToDetails = (item) => {
+    navigation.navigate('Item Details', {name: item.name, image: item.image});
+  }
+
+  const itemCell = ( item, index ) => (
+    <TouchableOpacity 
+      style={styles.cell}
+      onPress={() => goToDetails(item)}
+      key={index}
+    >
+      <Image style={styles.image} source={{ uri: 'data:image/jpeg;base64,' + item.image }} />
+    </TouchableOpacity>
+  )
+
+  return (
+    <SafeAreaView style={styles.grid}>
+      <ScrollView 
+        contentContainerStyle={styles.grid}
+        data={items}
+      >
+        {
+          items.map( (item, index) => 
+            itemCell(item, index)
+          )
+        }
+      </ScrollView>
+
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+export default ItemList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  grid: {
+    flex: 1,
+    marginTop: 3,
+    marginLeft: 3,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    flexWrap: 'wrap',
+  },
+  title: {
+    fontSize: 24,
+    marginLeft: 20,
+  },
+  cell: {
+    width: 115,
+    height: 115,
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  image: {
+    width: 110,
+    height: 110,
+  }
+});
